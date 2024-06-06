2a24d34c66a931f62c2e7a9d6ff885c6a9162dfc
tatiana-vieira <tatiana-vieira@unifei.edu.br>
2024-06-06 09:37:14 -0300

    pronto

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,14 @@
 import os
+import multiprocessing
 
 bind = "0.0.0.0:8000"
-workers = 2
+workers = multiprocessing.cpu_count() * 2 + 1
+
 if os.getenv("FLASK_DEBUG", "false").lower() == "true":
     reload = True
 else:
     reload = False
+
+loglevel = 'debug'
+accesslog = '-'
+errorlog = '-'
